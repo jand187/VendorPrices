abbf5b8a9797910045018251391e9fd0c3f5d76b
Jan Daniel Andersen <jand187@gmail.com>
2016-07-01 11:37:18 +0200

    Added first test.

---
+++ PriceChecker/StandardParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PriceChecker
+{
+	public class StandardParser
+	{
+		private readonly Dictionary<string, int> priceList;
+
+		public StandardParser()
+		{
+			priceList = new Dictionary<string, int>
+			{
+				{@"\+\d+ to maximum Life", 3},
+				{@"\+\d+% to Fire Resistance", 1}
+			};
+		}
+
+		public PriceCalculation Parse(string item)
+		{
+			var price = new PriceCalculation();
+
+			foreach (var priceTag in priceList)
+			{
+				var match = Regex.Match(item, priceTag.Key);
+				if (match.Success)
+					price.AddLine(priceTag.Value, Currencies.AlterationShard);
+			}
+
+			return price;
+		}
+	}
+
+	public class PriceCalculation
+	{
+		private readonly List<KeyValuePair<Currencies, int>> lines;
+
+		public PriceCalculation()
+		{
+			lines = new List<KeyValuePair<Currencies, int>>();
+		}
+
+		public void AddLine(int amount, Currencies currency)
+		{
+			lines.Add(new KeyValuePair<Currencies, int>(currency, amount));
+		}
+
+		public int CalculateTotal()
+		{
+			return lines.Where(l => l.Key == Currencies.AlterationShard).Sum(l => l.Value);
+		}
+	}
+
+
+	public enum Currencies
+	{
+		AlterationShard
+	}
+}
---
+++ PriceCheckerTests/StandardParserTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using PriceChecker;
+using Xunit;
+
+namespace PriceCheckerTests
+{
+	public class StandardParserTests
+	{
+		[Fact]
+		public void ParseMods()
+		{
+			var item = GetResource("PriceCheckerTests.TestData.Kraken Tether.txt");
+			
+			var parser = new StandardParser();
+			var price = parser.Parse(item); 
+			
+			Assert.Equal(4, price.CalculateTotal());
+		}
+
+		private static string GetResource(string name)
+		{
+			var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
+			using (var reader = new StreamReader(stream))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+
+
+}
